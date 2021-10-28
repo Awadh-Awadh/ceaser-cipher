00dbb72388d8df1ad128af9b167c1c9565678352
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-10-28 13:34:34 +0300

    add ccode documentations

--- cipher.py
+++ cipher.py
@@ -1,13 +1,14 @@
-
+import art
+print(art.logo)
 
 alphabet = [
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
              'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+              'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g',
+               'h', 'i', 'j', 'k',
+             'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
               'w', 'x', 'y', 'z'
               ]
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type the message:\n").lower()
-steps = int(input("Type the shift number:\n"))
 
 
 def ceaser(start_text, shift_amount, cipher_direction):
@@ -27,27 +28,27 @@ def ceaser(start_text, shift_amount, cipher_direction):
     
     
     '''
-
-
-
-
-
     cipher_text = ''
     plain_text = ''
     for letter in start_text:
-        position = alphabet.index(letter)
-        if cipher_direction == 'encode':
-            new_position = position + shift_amount
-            if new_position > 26:
-                new_pos = new_position - 26
-                letter = alphabet[new_pos] 
-            elif new_position < 26:
+        # check and shift letters in the allphabet list
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            if cipher_direction == 'encode':
+                new_position = (position + shift_amount) % 26
+
                 letter = alphabet[new_position]           
-            cipher_text += letter
+                cipher_text += letter
     
-        elif cipher_direction == 'decode':
-            new_position = position - shift_amount
-            plain_text += alphabet[new_position]
+            elif cipher_direction == 'decode':
+                new_position = (position - shift_amount) % 26
+                plain_text += alphabet[new_position]
+        # when a word contains either specials symbols or words they get attached to the end of the letter       
+        else:
+            cipher_text += letter
+            plain_text+=letter
+       
+
     if cipher_direction == 'encode':
       print(f"The encoded text is {cipher_text}")
     elif cipher_direction == 'decode':
@@ -55,9 +56,26 @@ def ceaser(start_text, shift_amount, cipher_direction):
     else:
         print("Choose either encode or decode !!!")
 
-            
 
-ceaser(text,steps,direction)
+# A loop that allows the function to run whether theuser wants to continue or not        
+should_end = True
+while should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type the message:\n").lower()
+    steps = int(input("Type the shift number:\n"))
+
+    ceaser(text, steps, direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == 'no':
+        should_end = False
+        print('Goodbye')
+
+
+
+
+
+
 
 
 
